26d022d59915e1691702c13874d3ae30d4fe0ac8
hardik0001 <hardik.bha65@gmail.com>
2022-10-08 03:28:25 +0530

    practise

--- practise_question/Product_of_Array_Except _self.cpp
+++ practise_question/Product_of_Array_Except _self.cpp
@@ -1,32 +1,45 @@
 #include<bits/stdc++.h>
 using namespace std;
-void prod(int arr[],int n ){
-    
-    for(int i=0;i<n;i++){
-        int p=1;
-        for(int j=0;j<n;j++){
-            if(arr[i]==arr[j]){
-                continue;
+void prod(long arr[],int n ){
+    long p=1;
+    long flag=0;
+    for(int i=0;i<n;i++)
+    {
+         if(arr[i]==0){   
+                flag++;
             }
-            else{
+            else {
                 p=p*arr[i];
             }
-            cout<<p;
-        }
-        
     }
-    
-}
+  
+    for(int i=0;i<n;i++)
+    {   
+           if(flag>1){
+               arr[i]=0;
+           }
+           else if(flag==1 && arr[i]!=0) {
+               arr[i]=0;
+           }
+           else if (flag==0){
+               arr[i]=p/arr[i];
 
+           }
+           /*else{
+               arr[i]=p;
+               
+           }*/
+           cout<<arr[i]<<" ";
+    }    
+}
 int main(){
-    int n;
-    cin>>n,no;
-    vector<int>v;
-    int arr[n];
+    long n;
+    cin>>n;
+    long arr[n];
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-    cin>>no;
     prod(arr,n);
+    
     
 }
