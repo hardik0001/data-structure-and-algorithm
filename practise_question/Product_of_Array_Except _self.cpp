f8b33f9f92f1a6b5dda4a36c7f2cfb31a78a2fa7
hardik0001 <hardik.bha65@gmail.com>
2022-10-08 19:59:27 +0530

    practise

--- practise_question/Product_of_Array_Except _self.cpp
+++ practise_question/Product_of_Array_Except _self.cpp
@@ -1,45 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
-void prod(long arr[],int n ){
-    long p=1;
-    long flag=0;
-    for(int i=0;i<n;i++)
-    {
-         if(arr[i]==0){   
-                flag++;
-            }
-            else {
-                p=p*arr[i];
-            }
-    }
-  
-    for(int i=0;i<n;i++)
-    {   
-           if(flag>1){
-               arr[i]=0;
-           }
-           else if(flag==1 && arr[i]!=0) {
-               arr[i]=0;
-           }
-           else if (flag==0){
-               arr[i]=p/arr[i];
 
-           }
-           /*else{
-               arr[i]=p;
-               
-           }*/
-           cout<<arr[i]<<" ";
-    }    
-}
 int main(){
-    long n;
+    long n,no;
     cin>>n;
     long arr[n];
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-    prod(arr,n);
-    
+    cin>>no;
+    int a[no]
+    for(int i=0;i<no;i++){
+        cin>>a[i]
+    }
+
+    // 1 2 3 4 5
+    int sum[i];
+    for(int i=0;i<n;i++){
+        if(a[i]==0){
+            sum[i]=arr[i]+arr[i]
+        }
+        else{
+            sum[i]=arr[n]+arr[i]
+        }
+    }
     
 }
--- practise_question/Rain _Water_Trapping.cpp
+++ practise_question/Rain _Water_Trapping.cpp
@@ -1,18 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
-void gap(int arr[],int n){
-    int max=0,sun=0;
 
-    for(int i=0;i<n;i++){
-        for(int j=i+2;j<=n;j++){
-            if(arr[i]>arr[j]){
-                sum+=(arr[i]-arr[j]);
-            }
-        }
+// 0  1  0  2  1  0  1  3  2  1  2  1
+void gap(int arr[],int n){
+    int sum=0;
+    int left[n];
+    int right[n];
+    left[0]=arr[0];
+    for(int i=1;i<n;i++)
+    {
+        left[i]= max( left[i-1] , arr[i] );  
     }
     
+    right[n-1]=arr[n-1];
+    for(int i=n-2;i>=0;i--)
+    {
+        right[i]=max(right[i+1],arr[i]);  
+    }
     
-    
+    for(int j=1;j<n-1;j++){
+        int mini=min(left[j-1],right[j+1]);
+        if(mini>arr[j]){
+            sum+=mini-arr[j];
+        }
+    }
+    cout<<sum<<endl;
+
 }
 
 int main(){
@@ -29,4 +42,4 @@ int main(){
         }
         gap(arr,no);
     }
-}cd
+}
