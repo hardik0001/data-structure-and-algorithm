48a04708251a84edc3edfc0bb483b4212346140e
hardik0001 <hardik.bha65@gmail.com>
2022-10-09 03:48:34 +0530

    practise

--- practise_question/cal_the_sum.cpp
+++ practise_question/cal_the_sum.cpp
@@ -1,5 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
+    int n;
+    int sum=0,mod=1e9+7;
+    cin>>n;
+    int arr[n];
+     int temp[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int no;
+    cin>>no;
+    for(int i=0;i<no;i++){
+        int x;
+        cin>>x;
+       for(int j=0;j<n;i++){
+        temp[j]= arr[j] + arr[(j-x+n)%n];
+
+       }
+       for(int i=0;i<n;i++)
+       {
+        temp[i]=arr[i];
+        }
+
+    }
+    for(int i=0;i<n;i++){
+        sum+=temp[i];
+    }
+    cout<<sum%mod;
+    
     
 }
---
+++ practise_question/palindrome.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+   
+    char str3[5]={"ABCD"};
+    while(5!=0){
+        int rem=str3%10;
+        sum=sum*10
+    }
+}
