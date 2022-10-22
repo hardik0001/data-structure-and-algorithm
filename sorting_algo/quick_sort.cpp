2645d88d6faf92be73989583edc031a8f5135659
hardik0001 <hardik.bha65@gmail.com>
2022-10-23 00:23:51 +0530

    quick sort

--- sorting_algo/quick_sort.cpp
+++ sorting_algo/quick_sort.cpp
@@ -6,7 +6,7 @@ int partition(int arr[],int low,int high){
     int pivot=arr[high];
     int i=low-1;
     for(int j=low;j<high;j++){
-        if(arr[i]<pivot){
+        if(arr[j]<pivot){
             i++;
             swap(arr[i],arr[j]);
         }
